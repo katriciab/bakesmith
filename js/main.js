a5bee46b16d753b76cbab51cbd351a4b5db5f577
Meow <Meow@Dev-Floater-92.local>
2014-02-23 23:53:12 -0500

    KatriciaB - Added in the Bakery service page! All the services will have similar layouts.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+$(document).ready(function() {
+	var servicesScrollPos = document.getElementById("services").scrollTop;
+	var scrollToTopDiv = $('.scroll-to-top');
+	scrollToTopDiv.hide();
 
+	var scrollTopFunction = function() {
+		var browserScrollPos = $(document).scrollTop();
+		if (browserScrollPos == 0) {
+			scrollToTopDiv.fadeOut();
+		} else if (browserScrollPos > servicesScrollPos) {
+			scrollToTopDiv.fadeIn();
+		}
+	};
+
+	$(window).scroll(scrollTopFunction);
+});
