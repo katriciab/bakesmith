3acf678bacb86ac4d38e066d94b057ddeb2e9f53
Katricia Barleta <katricia.barleta@gmail.com>
2014-03-09 20:21:53 -0400

    KatriciaB - Basic scope instagram integration - Changed services back navigation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+	<!-- Natural language input script -->
+		var nlform = new NLForm(document.getElementById('nl-form'));
+
+	<!-- Instagram script -->
+	var pictureLimit = (window.innerWidth > 992) ? 20 : 2;
+	var feed = new Instafeed({
+		get: 'user',
+		userId: 1163706720,
+		accessToken: '1163706720.4d3eb81.924672c4dbe54566a9a0860cd06dbcdd',
+		resolution: 'thumbnail',
+		limit: pictureLimit,
+		template: '<a href="{{link}}" target="_blank"><img src="{{image}}" class="instapic"/></a>'
+	});
+	feed.run();
+
+	<!-- Scroller script -->
 	var servicesScrollPos = document.getElementById("services").scrollTop;
 	var scrollToTopDiv = $('.scroll-to-top');
 	scrollToTopDiv.hide();
